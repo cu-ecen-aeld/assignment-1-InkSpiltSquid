8376b82b7feaab82e486d1f42c2cb96baf03cc10
InkSpiltSquid <robinsonab89@gmail.com>
2025-12-13 12:54:04 -0800

    created writer.sh and updated as instructed

---
+++ finder-app/writer.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#
+
+writefile=/tmp/aesd/assignment1
+writestr=AELD_IS_FUN
+
+if [ $# -lt 2 ];
+then
+    echo "Missing info"
+    exit 1
+fi
+
+if [ ! -d "$writefile"];
+then
+    echo "Directory not found"
+    exit 1
+fi
