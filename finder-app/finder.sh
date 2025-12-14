187c555e33878115f929b04ee25584528c932f23
InkSpiltSquid <robinsonab89@gmail.com>
2025-12-13 21:19:53 -0800

    updated finder.sh to take in arguments 1 and 2 see if the first argument is created, then check the number of files and check the matching lines within those files. then echo's the number of files and nummatchs
    Updated writer.sh to take in arguments 1 and 2, check if there are more than 2 arguements, if so exit 1. Creates the direction of arguement 1, if the director is not created, will have an exit 1. Then echos the second argument and pushs that string to the file created in arugment 1. if that operation is successful will have an exit 0, if not will have an exit 1.
    
    both ./finder-test.sh and ./full-test.sh successfully ran and completed without errors.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #searching files for a string
 
-filesdir=/tmp/aesd/assignment1/
-searchstr=AELD_IS_FUN
+
 
 if [ $# -lt  2 ];
 then
@@ -10,8 +9,15 @@ then
     exit 1
 fi
 
+filesdir=$1
+searchstr=$2
+
 if [ ! -d "$filesdir" ];
 then
-    echo "Directory not found"
+    echo "$filesdir not found"
     exit 1
 fi
+
+numfiles=$(find "$filesdir" -type f | wc -l)
+nummatches=$(grep -rl "$searchstr" "$filesdir" | wc -l)
+echo "The number of files are $numfiles and the number of matching lines are $nummatches"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Writing to files
 
-writefile=/tmp/aeld-data
-writestr=AELD_IS_FUN
+writefile=$1
+writestr=$2
 
 if [ $# -lt 2 ];
 then
@@ -10,8 +10,22 @@ then
     exit 1
 fi
 
-if [ ! -d $writefile ];
+
+
+writedr=$(dirname "$writefile")
+mkdir -p "$writedr"
+
+if [ ! -d "$(dirname "$writefile")" ];
 then
-    echo "Writer Directory not found"
+    echo "Directory $(dirname "$writefile") not found"
+    exit 1
+fi
+
+if echo "$writestr" > "$writefile";
+then
+    echo "Wrote string to $writefile"
+    exit 0
+else
+    echo "Failed to write string to $writefile"
     exit 1
 fi
